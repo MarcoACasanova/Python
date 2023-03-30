f10e921010f6020cb04137880cf7ba1f38e8352b
Marco Antônio Casanova <masucasanova@gmail.com>
2023-03-30 13:43:34 -0300

    Update fibonacci.py
    Deixando o código mais curto

--- Exercicios/fibonacci.py
+++ Exercicios/fibonacci.py
@@ -3,17 +3,8 @@ def fib(n):
         return None
     if n < 3:
         return 1
-
-    elem_1 = elem_2 = 1
-    the_sum = 0
-    for i in range(3, n + 1):
-        the_sum = elem_1 + elem_2
-        elem_1, elem_2 = elem_2, the_sum
-    return the_sum
+    return fib(n - 1) + fib(n - 2)
 
 
-for n in range(1, 10):  # testing
+for n in range(1, 10): #testing
     print(n, "->", fib(n))
-
-
-
