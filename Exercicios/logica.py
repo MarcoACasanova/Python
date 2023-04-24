29288801d6c97742b86f954118fc2eeef3029519
Marco Antônio Casanova <masucasanova@gmail.com>
2023-04-24 11:42:47 -0300

    Teste de lógica
    24/abr

---
+++ Exercicios/logica.py
@@ -0,0 +1,12 @@
+peca_1 = int(input('Digite o número da peça desejada: '))
+qntd_1 = float(input('Digite a quantidade desejada'))
+
+valor_1 = 5.30
+valor_2 = 5.10
+
+peca_2 = int(input('Digite o número desejado da segunda peça: '))
+qntd_2 = float(input('Digite a quantidade necessária da segunda peça: '))
+
+soma = (valor_1 * qntd_1) + (valor_2 * qntd_2)
+
+print(f'O valor a ser pago é de {soma}')
---
+++ Exercicios/raio.py
@@ -0,0 +1,7 @@
+import math
+
+raio = float(input('Digite o raio a ser calculado: '))
+
+area = math.pi * raio ** 2
+
+print(f'O resultado é de: {area:.4f}')
